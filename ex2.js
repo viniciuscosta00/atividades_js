b09da3a3708c218a08c1315f4053201e056086a1
Pedro Vinicius <viniacosta2020@gmail.com>
2025-06-26 15:12:29 -0300

    ex2

---
+++ ex2.js
@@ -0,0 +1,12 @@
+let num1 = 10
+let num2 = 2
+
+soma = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+console.log("A soma é: ", soma)
+console.log("A subtração é: ", sub)
+console.log("A multiplicação é: ", mul)
+console.log("A divisão é: ", div)
